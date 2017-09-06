9ca408e382d9a55b7cd0376b9d8d6b3186091351
Rafael Henrique <rafael.henriquemorais@gmail.com>
2017-09-06 17:59:39 -0300

    Criando repositorio de Pagamento

--- Source/CandyShop.Repository/PagamentoRepository.cs
+++ Source/CandyShop.Repository/PagamentoRepository.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CandyShop.Core.Pagamento.Dto;
 
 namespace CandyShop.Repository
 {
-    class PagamentoRepository
+    public class PagamentoRepository : ConnectDB
     {
+        private enum Procedures
+        {
+            GCS_InsPagamento,
+            GCS_UpdPagamento,
+            GCS_DelPagamento,
+            GCS_LisPagamento,
+            GCS_LisCpfPagamento
+        }
+        private void Inserir(Pagamento pagamento)
+        {
+            ExecuteProcedure(Procedures.GCS_InsPagamento);
+            AddParameter("IdPagamento", pagamento.IdPagamento);
+            AddParameter("UsuarioPagamento", pagamento.Usuario);
+            AddParameter("DataPagamento", pagamento.DataPagamento);
+            AddParameter("ValorPagamento", pagamento.ValorPagamento);
+
+            ExecuteNonQuery();
+        }
+
     }
 }
