be950df477afc2244ac7b69bd7539124c196aa07
joaotostes <joao_tostes17@hotmail.com>
2017-09-06 17:22:38 -0300

    Repositório do Produto pronto

--- Source/CandyShop.Repository/ProdutoRepository.cs
+++ Source/CandyShop.Repository/ProdutoRepository.cs
@@ -5,7 +5,7 @@ namespace CandyShop.Repository
 
 
 {
-    public class ProdutoRepository : ConnectDB 
+    public class ProdutoRepository : ConnectDB
     {
         private enum Procedures
         {
@@ -18,9 +18,36 @@ namespace CandyShop.Repository
         public void InserirProduto(ProdutoDto produto)
         {
             ExecuteProcedure(Procedures.GCS_InsProduto);
-            AddParameter("NomeProduto",produto.NomeProduto);
-            AddParameter("PrecoProduto",produto.PrecoProduto);
-            AddParameter("QtdeProduto",produto.QtdeProduto);
+            AddParameter("NomeProduto", produto.NomeProduto);
+            AddParameter("PrecoProduto", produto.PrecoProduto);
+            AddParameter("QtdeProduto", produto.QtdeProduto);
+
+            ExecuteNonQuery();
+        }
+
+        public void DeletaProduto(int idproduto)
+        {
+            ExecuteProcedure(Procedures.GCS_DelProduto);
+            AddParameter("IdProduto", idproduto);
+            ExecuteNonQuery();
+        }
+
+        public void UpdateProduto(ProdutoDto produto)
+        {
+            ExecuteProcedure(Procedures.GCS_UpdProduto);
+            AddParameter("IdProduto", produto.IdProduto);
+            AddParameter("NomeProduto", produto.NomeProduto);
+            AddParameter("PrecoProduto", produto.PrecoProduto);
+            AddParameter("QtdeProduto", produto.QtdeProduto);
+            ExecuteNonQuery();
+        }
+
+        public bool SelecionarProdutos(int idproduto)
+        {
+            ExecuteProcedure(Procedures.GCS_SelProduto);
+            AddParameter("IdProduto", idproduto);
+            using (var retornobd = ExecuteReader())
+                return retornobd.Read();
         }
     }
 }
