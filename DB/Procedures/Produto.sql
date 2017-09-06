880b3110a137109c055e81e054ef93ab60c7849b
joaotostes <joao_tostes17@hotmail.com>
2017-09-06 13:59:48 -0300

    Procedure CRUD da tabela Produto finalizado

--- DB/Procedures/Produto.sql
+++ DB/Procedures/Produto.sql
@@ -32,7 +32,7 @@ IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[GCS_DelPro
 GO
 
 CREATE PROCEDURE [dbo].[GCS_DelProduto]
-
+	@IdProduto int
 	AS
 
 	/*
@@ -46,12 +46,68 @@ CREATE PROCEDURE [dbo].[GCS_DelProduto]
 	*/
 
 	BEGIN
-	
-		
-
-
+	DELETE [dbo].[Produto]
+		WHERE IdProduto = @IdProduto		
 	END
 GO
+
+
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[GCS_UpdProduto]') AND objectproperty(id, N'IsPROCEDURE')=1)
+	DROP PROCEDURE [dbo].[GCS_UpdProduto]
+GO
+
+CREATE PROCEDURE [dbo].[GCS_UpdProduto]
+	@IdProduto int,
+	@NomeProduto varchar(40),
+	@PrecoProduto decimal,
+	@QtdeProduto int
+	AS
+
+	/*
+	Documentação
+	Arquivo Fonte.....: Produto.sql
+	Objetivo..........: Editar Poodutos
+	Autor.............: SMN - João Guilherme
+ 	Data..............: 06/09/2017
+	Ex................: EXEC [dbo].[GCS_UpdProduto]
+
+	*/
+
+	BEGIN
+		UPDATE [dbo].[Produto]
+		SET	NomeProduto = @NomeProduto,
+			PrecoProduto = @PrecoProduto,
+			QtdeProduto = @QtdeProduto
+		WHERE IdProduto = @IdProduto
+	END
+GO
+		
+		
+		
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[GCS_SelProduto]') AND objectproperty(id, N'IsPROCEDURE')=1)
+	DROP PROCEDURE [dbo].[GCS_SelProduto]
+GO
+
+CREATE PROCEDURE [dbo].[GCS_SelProduto]
+	@IdProduto int
+	AS
+
+	/*
+	Documentação
+	Arquivo Fonte.....: Produto.sql
+	Objetivo..........: Selecionar Produtos
+	Autor.............: SMN - João Guilherme
+ 	Data..............: 06/09/2017
+	Ex................: EXEC [dbo].[GCS_SelProduto]
+
+	*/
+
+	BEGIN
+		SELECT * FROM Produto 
+			WHERE IdProduto = @IdProduto
+	END
+GO
+						
 
 
 
