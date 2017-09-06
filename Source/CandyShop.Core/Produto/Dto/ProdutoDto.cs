491ccb3ef72522b67243cd71f3152d01c69877f3
joaotostes <joao_tostes17@hotmail.com>
2017-09-06 17:00:34 -0300

    Repositório e Dto de Produto Criado

---
+++ Source/CandyShop.Core/Produto/Dto/ProdutoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CandyShop.Core.Services.Produto.Dto
+{
+    public class ProdutoDto
+    {
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public decimal PrecoProduto { get; set; }
+        public int QtdeProduto { get; set; }
+    }
+}
--- Source/CandyShop.Repository/ProdutoRepository.cs
+++ Source/CandyShop.Repository/ProdutoRepository.cs
@@ -1,7 +1,26 @@
-﻿namespace CandyShop.Repository
+﻿
+using CandyShop.Core.Services.Produto.Dto;
+
+namespace CandyShop.Repository
+
+
 {
     public class ProdutoRepository : ConnectDB 
     {
-        
+        private enum Procedures
+        {
+            GCS_InsProduto,
+            GCS_DelProduto,
+            GCS_UpdProduto,
+            GCS_SelProduto
+        }
+
+        public void InserirProduto(ProdutoDto produto)
+        {
+            ExecuteProcedure(Procedures.GCS_InsProduto);
+            AddParameter("NomeProduto",produto.NomeProduto);
+            AddParameter("PrecoProduto",produto.PrecoProduto);
+            AddParameter("QtdeProduto",produto.QtdeProduto);
+        }
     }
 }
