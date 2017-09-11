8c83547d3a187ed6f9f16e4322fca4f208a617cf
joaotostes <joao_tostes17@hotmail.com>
2017-09-11 14:06:09 -0300

    Update no service do usuario

--- Source/CandyShop.Core/Services/Usuario/UsuarioService.cs
+++ Source/CandyShop.Core/Services/Usuario/UsuarioService.cs
@@ -1,15 +1,15 @@
-﻿using CandyShop.Core.Infra;
+﻿using CandyShop.Core.Services._Interfaces;
 using CandyShop.Core.Usuario.Dto;
 
 namespace CandyShop.Core.Services.Usuario
 {
     public class UsuarioService : IUsuarioService
     {
-        private readonly Notification _notification;
+        private readonly INotification _notification;
         private readonly IUsuarioRepository _usuarioRepository;
 
 
-        public UsuarioService(Notification notification, IUsuarioRepository usuarioRepository)
+        public UsuarioService(INotification notification, IUsuarioRepository usuarioRepository)
         {
             _notification = notification;
             _usuarioRepository = usuarioRepository;
@@ -17,8 +17,6 @@ namespace CandyShop.Core.Services.Usuario
 
         public void InserirUsuario(UsuarioDto usuario)
         {
-            if (_notification.HasNotification())
-                return;
             _usuarioRepository.InserirUsuario(usuario);
         }
     }
