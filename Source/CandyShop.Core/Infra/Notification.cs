ae1503ca68a41389abcad7064dcc802871d13435
joaotostes <joao_tostes17@hotmail.com>
2017-09-11 10:18:14 -0300

    Infra core terminado (Notifications)

---
+++ Source/CandyShop.Core/Infra/Notification.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CandyShop.Core.Infra
+{
+    class Notification
+    {
+        //Criando uma lista para guardar as notification
+        private readonly List<string> _notifications;
+
+        //atribuindo a variavel _notifications com a lista 
+        public Notification()
+        {
+          _notifications = new List<string>(); 
+        }
+
+        //Método pra adicionar notification no sistema caso precisar
+        public void Add(string notification)
+        {
+            _notifications.Add(notification);
+        }
+
+        //Verifica se tem notification na lista (serve para verificações posteriores)
+        public bool HasNotification => _notifications != null && _notifications.Any();
+
+        //Pega alguam notification dentro da lista 
+        public IEnumerable<string> GetNotification => _notifications;
+    }
+}
---
+++ Source/CandyShop.WebAPI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CandyShop.WebAPI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
