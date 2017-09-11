d8c5cd0a971f100d617aa3bba1947fdb94218cf4
Rafael Henrique <rafael.henriquemorais@gmail.com>
2017-09-11 10:32:02 -0300

    otimizando e comentando o arquivo ajax

--- Source/CandyShop.Web/Js/Ajax.js
+++ Source/CandyShop.Web/Js/Ajax.js
@@ -1,51 +1,52 @@
 ﻿var AjaxJs = (function() {
-    var url = {};
+    var url = {};   //objeto que recebe o nome e endereço da pagina
 
+    // Lista de objetos que guarda o nome e o endereco da pagina, sã carregados na pagina padrao
     var init = function(config) {
         url = config;
         main();
     };
 
-    function main() {        //carrega a pagina principal
-        $.get(url.main).success(function(data) {
-            $('#DivGrid').html(data).slideDown(function() { //desce  o divgrid
-                //if (mensagem)
-                    //$(this).successMessage(mensagem, 5000); //informa mensagem de sucesso                
+    //carrega a pagina de inicio
+    function main() {
+        $.get(url.main).success(function(data) {                //pega a view main e a carrega no div
+            $("#DivGrid").slideUp(function() {
+                $('#DivGrid').hide().html(data).slideDown();    //desce  o divgrid                                                                                
+            }).error(function(xhr) {                            //xhr é o código do erro, que é retornado caso o get não tenha sucesso
+                $("#DivGrid").errorMessage(xhr.responseText);
             });
-        }).error(function(xhr) {
-            $("#DivGrid").errorMessage(xhr.responseText);
         });
-    };
+    }
 
-    function chamaPagina(endereco) {
-        $.get(endereco).success(function (data) {
-            $('#DivGrid').slideUp(function () {
-                $('#DivGrid').hide().html(data).slideDown();
+    //Função genérica para carregar o div, de acordo com o endereço passado
+    function chamaPagina(endereco) {    
+        $.get(endereco).success(function (data) {               //data é o conteudo da view
+            $('#DivGrid').slideUp(function () {                 //a div é recolhida
+                $('#DivGrid').hide().html(data).slideDown();    //escondida, carregada e demonstrada novamente
             });
         }).error(function (xhr) {
             $('#DivGrid').errorMessage(xhr.responseText);            
         });
     };
 
+    //Variavel que retorna para o inicio
     var voltarInicio = function () {
-        $.get(url.main).success(function () {
-            $("#DivGrid").slideUp(function () {
-                $("#DivGrid").empty(); //$(this).empty();                
-                main();
-            }).error(function (xhr) {
-                $("#DivGrid").errorMessage(xhr.responseText);
-            });
-        });
+        main();
     };
 
+    //var que chama a view pagamento
     var pagamento = function () {
         chamaPagina(url.pagamento);
     };
 
+    //var que chama a view historico de compra
     var historicoCompra = function () {
         chamaPagina(url.historicoCompra);
     };
 
+
+
+    //retorna links para acessar as paginas.
     return {
         init: init,
         pagamento: pagamento,
@@ -53,4 +54,4 @@
         historicoCompra: historicoCompra
     };
 
-})();
+})();           //O método ajaxJS é auto executado quando é iniciado o sistema.
