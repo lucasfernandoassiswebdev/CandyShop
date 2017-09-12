a1a0a1ab38d4fd02337d7101100f3bbee7daa0b4
joaotostes <joao_tostes17@hotmail.com>
2017-09-12 17:15:24 -0300

    ProdutoControlle em andamento

--- Source/CandyShop.WebAPI/Controllers/ProdutoController.cs
+++ Source/CandyShop.WebAPI/Controllers/ProdutoController.cs
@@ -1,13 +1,28 @@
-﻿using System.Web.Http;
+﻿using CandyShop.Core.Services._Interfaces;
+using CandyShop.Core.Services.Produto;
+using CandyShop.Core.Services.Produto.Dto;
+using System.Web.Http;
 
 namespace CandyShop.WebAPI.Controllers
 {
     public class ProdutoController : ApiController
     {
-        // GET: Produto
-        //public ActionResult Index()
-        //{
-           
-        //}
+        private readonly INotification _notification;
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+
+
+        public ProdutoController(INotification notification, IProdutoRepository produtoRepository,
+            IProdutoService produtoService)
+        {
+            _notification = notification;
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+        }
+
+        IHttpActionResult PostProduto(ProdutoDto produto)
+        {
+            
+        }
     }
 }
