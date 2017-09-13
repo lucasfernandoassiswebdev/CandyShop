4e8b8db16b2010422701915e8d46553249d5bda1
Lucas Fernando <lucasaassis@outlook.com>
2017-09-13 10:39:27 -0300

    Adicionando validações na tela de cadastro de usuário

---
+++ Source/CandyShop.Web/Js/Usuario/scriptsCadastroUsuario.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    $('input').characterCounter();
+
+    $('#cpf').on('keydown', function () {
+        mcpf($('#cpf').val());
+    });
+
+    $('#cpf').on('blur', function () {
+        if ($('#cpf').val().length > 14) {
+            $('#cpf').val($('#cpf').val().substr(0, 13));
+            $('#cpf').keydown();
+        }
+    });
+
+    $('#Nome').on('keydown', function () {
+        var qtde = $('#Nome').val().length;
+
+        if (qtde > 50 || qtde == 0) {
+            $('.waves ').attr('disabled',true);
+        } else {
+            $('.waves').removeAttr('disabled');
+        }
+    });
+
+    $('#Nome').on('blur', function () {
+        var qtde = $('#Nome').val().length;
+
+        if (qtde > 50 || qtde === 0) {
+            $('.waves ').attr('disabled',true);
+        } else {
+            $('.waves').removeAttr('disabled');
+        }
+    });
+});
+
+function mcpf(v) {
+    v = v.replace(/\D/g, "");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    $('#cpf').val(v);
+}
