b470a7f1a60df4c8151f2d9143bbe1b8cac732aa
Lucas Fernando <lucasaassis@outlook.com>
2017-09-13 11:19:16 -0300

    Centralizando textos

--- Source/CandyShop.Web/Js/scriptsAdmin.js
+++ Source/CandyShop.Web/Js/scriptsAdmin.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.collapsible').collapsible();
+    $('.modal').modal();
 
     $(".dropdown-button").on('click',function () {
         $('.button-collapse').sideNav('hide');
