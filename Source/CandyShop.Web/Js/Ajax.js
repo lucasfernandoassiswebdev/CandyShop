e36c19ba3315190943b20bdb4f575074b9857a2a
Rafael Henrique <rafael.henriquemorais@gmail.com>
2017-09-13 09:21:04 -0300

    Pagina de cadastro de produto no ajax

--- Source/CandyShop.Web/Js/Ajax.js
+++ Source/CandyShop.Web/Js/Ajax.js
@@ -1,33 +1,49 @@
-﻿var AjaxJs = (function($) {
+﻿var AjaxJs = (function ($) {
     var url = {};   //objeto que recebe o nome e endereço da pagina
 
     // Lista de objetos que guarda o nome e o endereco da pagina, sã carregados na pagina padrao
-    var init = function(config) {
+    var init = function (config) {
         url = config;
         main();
     };
 
     //carrega a pagina de inicio
     function main() {
-        $.get(url.main).done(function(data) {                //pega a view main e a carrega no div
-            $("#DivGrid").slideUp(function() {
+        $.get(url.main).done(function (data) {                //pega a view main e a carrega no div
+            $("#DivGrid").slideUp(function () {
                 $('#DivGrid').hide().html(data).slideDown(); //desce  o divgrid                                                                                
             });
-        }).fail(function(xhr) {                            //xhr é o código do erro, que é retornado caso o get não tenha sucesso
+        }).fail(function (xhr) {                            //xhr é o código do erro, que é retornado caso o get não tenha sucesso
             $("#DivGrid").errorMessage(xhr.responseText);
         });
     }
 
     //Função genérica para carregar o div, de acordo com o endereço passado
-    function chamaPagina(endereco) {    
+    function chamaPagina(endereco) {
         $.get(endereco).done(function (data) {               //data é o conteudo da view
             $('#DivGrid').slideUp(function () {                 //a div é recolhida
                 $('#DivGrid').hide().html(data).slideDown();    //escondida, carregada e demonstrada novamente
             });
         }).fail(function (xhr) {
-            $('#DivGrid').errorMessage(xhr.responseText);            
+            $('#DivGrid').errorMessage(xhr.responseText);
         });
     };
+
+    var concluirCadastroProduto = function () {
+        $.post(url.concluirCadastroProduto,
+            {
+                NomeProduto: $('#NomeProduto').val(),
+                PrecoProduto: $('#PrecoProduto').val(),
+                QtdeProduto: $('#QtdeProduto').val()
+            }).done(function() {                        //passar o parametro data aqui quando for definida a mensagem
+            $('#DivGrid').slideUp(function() {
+                $(this).empty();
+                chamaPagina(url.cadastrarProduto);
+            });
+        }).fail(function(xhr) {
+            console.log(xhr.responseText);
+        });
+    }
 
     //Variavel que retorna para o inicio
     var voltarInicio = function () {
@@ -41,32 +57,37 @@
     var historicoCompra = function () {
         chamaPagina(url.historicoCompra);
     };
-    var listaUsuario = function() {
+    var listaUsuario = function () {
         chamaPagina(url.listaUsuario);
     }
-    var listaProduto = function() {
+    var listaProduto = function () {
         chamaPagina(url.listaProduto);
     }
-    var detalhePagamento = function() {
+    var detalhePagamento = function () {
         chamaPagina(url.detalhePagamento);
     };
-    var mostraSaldo = function() {
+    var mostraSaldo = function () {
         chamaPagina(url.mostraSaldo);
     };
-    var cadastrarProduto = function() {
+    var cadastrarProduto = function () {
         chamaPagina(url.cadastrarProduto);
     }
     //retorna links para acessar as paginas.
     return {
+        //para admin  e usuario
         init: init,
-        pagamento: pagamento,
         voltarInicio: voltarInicio,
+        //usuario
+        pagamento: pagamento,
+        detalhePagamento: detalhePagamento,
         historicoCompra: historicoCompra,
+        
+        //admin
         listaUsuario: listaUsuario,
         listaProduto: listaProduto,
-        mostraSaldo: mostraSaldo,
-        detalhePagamento: detalhePagamento,
-        cadastrarProduto: cadastrarProduto
-};       
+        mostraSaldo: mostraSaldo,        
+        cadastrarProduto: cadastrarProduto,
+        concluirCadastroProduto: concluirCadastroProduto
+    };
 
 })(jQuery);           //O método ajaxJS é auto executado quando é iniciado o sistema.
