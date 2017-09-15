d90778067e9c306239f0dca9740499da53501883
Lucas Fernando <lucasaassis@outlook.com>
2017-09-15 16:00:42 -0300

    Começando a adicionar injeção de dependencia na camada web

---
+++ Source/CandyShop.Web/App_Start/SimpleInjectorContainer.cs
@@ -0,0 +1,17 @@
+﻿using SimpleInjector;
+using SimpleInjector.Lifestyles;
+
+namespace CandyShop.Web.App_Start
+{
+    public class SimpleInjectorContainer
+    {
+        public static Container Build()
+        { 
+        var container = new Container();
+        container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            container.Verify();
+            return container;
+        }
+    }
+}
