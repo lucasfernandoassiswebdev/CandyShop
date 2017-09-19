60c7a01f6155c586df9c2c6840ac55a86c2b88ae
Lucas Fernando <lucasaassis@outlook.com>
2017-09-19 16:56:40 -0300

    Andando em círculos

--- Source/CandyShop.Web/Js/scriptsCadastroUsuario.js
+++ Source/CandyShop.Web/Js/scriptsCadastroUsuario.js
@@ -1,4 +1,5 @@
-﻿$('#fotoUsuario').change(function () {
+﻿//muda a foto do usuário na tela
+$('#fotoUsuario').change(function () {
     readURL(this);
 });
 
@@ -6,7 +7,7 @@ function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             $('#imagem').attr('src', e.target.result);
         };
 
@@ -14,41 +15,56 @@ function readURL(input) {
     }
 }
 
+//inicia os métodos que o materialize pede 
+//e desativa os botões de envio caso o formulário esteja inválido
 $(document).ready(function () {
     $('input').characterCounter();
+    $('.tooltipped').tooltip({ delay: 50 });
 
     $('#cpf').on('keydown', function () {
         mcpf($('#cpf').val());
     });
 
     $('#cpf').on('blur', function () {
+        //retirando caracteres a mais do campo
         if ($('#cpf').val().length > 14) {
             $('#cpf').val($('#cpf').val().substr(0, 13));
             $('#cpf').keydown();
         }
-    });
-
-    $('#Nome').on('keydown', function () {
-        var qtde = $('#Nome').val().length;
-
-        if (qtde > 50 || qtde === 0) {
-            $('.waves ').attr('disabled',true);
+        //validando o cpf
+        var cpfNew = $('#cpf').val();
+        cpfNew = cpfNew.replace('.', '');
+        cpfNew = cpfNew.replace('.', '');
+        cpfNew = cpfNew.replace('-', '');
+        if (TestaCPF(cpfNew)) {
+            $('.botaoCadastro ').prop('disabled', true);
         } else {
-            $('.waves').removeAttr('disabled');
-        }
-    });
-
-    $('#Nome').on('blur', function () {
-        var qtde = $('#Nome').val().length;
-
-        if (qtde > 50 || qtde === 0) {
-            $('.waves ').attr('disabled',true);
-        } else {
-            $('.waves').removeAttr('disabled');
+            $('.botaoCadastro ').prop('disabled', false);
         }
     });
 });
 
+$('#Nome').on('keydown', function () {
+    var qtde = $('#Nome').val().length;
+
+    if (qtde > 50 || qtde === 0) {
+        $('.botaoCadastro ').prop('disabled', true);
+    } else {
+        $('.botaoCadastro').removeAttr('disabled');
+    }
+});
+
+$('#Nome').on('blur', function () {
+    var qtde = $('#Nome').val().length;
+
+    if (qtde > 50 || qtde === 0) {
+        $('.botaoCadastro ').attr('disabled', true);
+    } else {
+        $('.botaoCadastro').removeAttr('disabled');
+    }
+});
+
+//função que remove caracteres inválidos do campo de CPF
 function mcpf(v) {
     v = v.replace(/\D/g, "");
     v = v.replace(/(\d{3})(\d)/, "$1.$2");
@@ -57,3 +73,27 @@ function mcpf(v) {
     $('#cpf').val(v);
 }
 
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    //strCPF  = RetiraCaracteresInvalidos(strCPF,11);
+    if (strCPF == "00000000000")
+        return false;
+    for (i = 1; i <= 9; i++)
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11))
+        Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)))
+        return false;
+    Soma = 0;
+    for (i = 1; i <= 10; i++)
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11))
+        Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11)))
+        return false;
+    return true;
+}
