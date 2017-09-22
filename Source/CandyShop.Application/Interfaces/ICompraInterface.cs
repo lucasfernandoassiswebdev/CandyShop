f44c0ce5c6450389adbc37837c762389f477cbee
Rafael Henrique de Morais <rafael.henriquemorais@gmail.com>
2017-09-21 21:04:30 -0300

    rodando novamente

--- Source/CandyShop.Application/Interfaces/ICompraInterface.cs
+++ Source/CandyShop.Application/Interfaces/ICompraInterface.cs
@@ -5,16 +5,12 @@ namespace CandyShop.Application.Interfaces
 {
     public interface ICompraInterface
     {
-        Response<string> InserirCompra(Compra compra);
-        Response<string> EditarCompra(Compra compra);
-        Response<string> InserirItens(Produto produto);
-        Response<IEnumerable<Compra>> ListaCompra();
-        Response<IEnumerable<Compra>> ListaCompraPorCpf(string cpf);
-        Response<Compra> SelecionarCompra(int idcompra);
-        Response<string> EditaItens();
-        Response<IEnumerable<Compra>> ListaCompraPorNome(string nomeUsuario);
-
-
-
+        Response<string> InserirCompra(CompraViewModel compra);
+        Response<string> EditarCompra(CompraViewModel compra);
+        Response<string> InserirItens(ProdutoViewModel produto);
+        Response<IEnumerable<CompraViewModel>> ListaCompra();
+        Response<IEnumerable<CompraViewModel>> ListaCompraPorCpf(string cpf);
+        Response<CompraViewModel> SelecionarCompra(int idcompra);        
+        Response<IEnumerable<CompraViewModel>> ListaCompraPorNome(string nomeUsuario);
     }
 }
