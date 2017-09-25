3d37b8a7cdcfb983e71014e57f15da16a42a53d5
Rafael Henrique <rafael.henriquemorais@gmail.com>
2017-09-25 17:58:08 -0300

    implementando listar compras

--- Source/CandyShop.Web/Controllers/CompraController.cs
+++ Source/CandyShop.Web/Controllers/CompraController.cs
@@ -1,18 +1,82 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using CandyShop.Application.Interfaces;
 
 namespace CandyShop.Web.Controllers
 {
     public class CompraController : Controller
     {
+        private readonly ICompraApplication _appCompra;
+
+        public CompraController(ICompraApplication compra)
+        {
+            _appCompra = compra;
+        }
+
+
         // GET: Compra
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Historico()
+        #region listas
+
+        public ActionResult Listar()
         {
-            return View();
+            ViewBag.tituloPagina = "Compras do ultimo mês";
+            ViewBag.drop = 0;
+            var response = _appCompra.ListaCompra();
+            if (response.Status != HttpStatusCode.OK)
+                return Content("Erro " + response.ContentAsString.First());
+            return View("Index", response.Content);
         }
+
+        public ActionResult ListarCpf()
+        {
+            ViewBag.tituloPagina = "Minhas Compras";
+            var cpf = Session["login"].ToString();
+            ViewBag.drop = 1;
+            var response = _appCompra.ListaCompraPorCpf(cpf);
+            if (response.Status != HttpStatusCode.OK)
+                return Content("Erro " + response.ContentAsString.First());
+            return View("Index", response.Content);
+        }
+
+        public ActionResult ListarSemana()
+        {
+            ViewBag.tituloPagina = "Compras da ultima semana";
+            ViewBag.drop = 1;
+            var response = _appCompra.ListarComprasSemana();
+            if (response.Status != HttpStatusCode.OK)
+                return Content("Erro " + response.ContentAsString.First());
+            return View("Index", response.Content);
+        }
+
+        public ActionResult ListarMes(int mes)
+        {
+            ViewBag.tituloPagina = $"Compra do mês {mes}";
+            ViewBag.drop = 0;
+            var response = _appCompra.ListarCompras(mes);
+            if (response.Status != HttpStatusCode.OK)
+                return Content("Erro " + response.ContentAsString.First());
+            return View("Index", response.Content);
+        }
+
+        public ActionResult ListarDia()
+        {
+            ViewBag.tituloPagina = $"Compras do dia {DateTime.Now.ToShortDateString()}";
+            ViewBag.drop = 1;
+            var response = _appCompra.ListarComprasDia();
+            if (response.Status != HttpStatusCode.OK)
+                return Content("Erro " + response.ContentAsString.First());
+            return View("Index", response.Content);
+        }        
+
+        
+
+        #endregion
     }
 }
