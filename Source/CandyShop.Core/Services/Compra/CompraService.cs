474a895d9332b64c61ebe8a7ef2843981c3d0c58
Lucas Fernando <lucasaassis@outlook.com>
2017-09-29 09:34:58 -0300

    Encapsulando endereço da API nos applications

--- Source/CandyShop.Core/Services/Compra/CompraService.cs
+++ Source/CandyShop.Core/Services/Compra/CompraService.cs
@@ -27,8 +27,7 @@ namespace CandyShop.Core.Services.Compra
             _compraRepository.BeginTransaction();
             try
             {
-                int valor = 0;
-                var result = _compraRepository.InserirCompra(compra, out valor);
+                var result = _compraRepository.InserirCompra(compra, out int valor);
                 if (result == -1)
                 {
                     _compraRepository.RollBackTransaction();
@@ -57,7 +56,7 @@ namespace CandyShop.Core.Services.Compra
             catch (Exception e)
             {
                 _compraRepository.RollBackTransaction();
-                _notification.Add("Erro ao inserir compra");
+                _notification.Add("Erro ao inserir compra " + e.Message);
                 return 0;
             }
         }
