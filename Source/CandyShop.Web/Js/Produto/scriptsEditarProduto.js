017974f9ccabb1db06d55c52c8796dafd680a29b
Lucas Fernando <lucasaassis@outlook.com>
2017-10-09 11:18:52 -0300

    adicionando máscara a edição de saldo de usuario

--- Source/CandyShop.Web/Js/Produto/scriptsEditarProduto.js
+++ Source/CandyShop.Web/Js/Produto/scriptsEditarProduto.js
@@ -151,5 +151,28 @@ function readURL3(input) {
     }
 }
 
+function FilterInput(event) {
+    var keyCode = ('which' in event) ? event.which : event.keyCode;
+    isNotWanted = (keyCode === 69 || keyCode === 189 || keyCode === 109);
+    return !isNotWanted;
+}
+
+function handlePaste(e) {
+    var clipboardData, pastedData;
+
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedData = clipboardData.getData('Text').toUpperCase();
+
+    if (pastedData.indexOf('E') > -1) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    if (pastedData.indexOf('-') > -1) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+}
+
 
 
