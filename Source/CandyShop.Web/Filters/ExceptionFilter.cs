16f5ca89bdebfd1700b07867d096946b1adf2580
Lucas Fernando <lucasaassis@outlook.com>
2017-10-09 16:23:40 -0300

    Adicionando filtro de exception

---
+++ Source/CandyShop.Web/Filters/ExceptionFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CandyShop.Web.Filters
+{
+    public class ExceptionFilter : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+        }
+    }
+}
