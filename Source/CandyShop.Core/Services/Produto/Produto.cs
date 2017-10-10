7432c5c2975badb9f83a3e401d14a525321fd60c
Lucas Fernando <lucasaassis@outlook.com>
2017-10-10 13:06:54 -0300

    Dto's substituidas por entidades

---
+++ Source/CandyShop.Core/Services/Produto/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace CandyShop.Core.Services.Produto
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public decimal PrecoProduto { get; set; }
+        public int QtdeProduto { get; set; }
+        public string Ativo { get; set; }
+        public string Categoria { get; set; }
+    }
+}
+
+
