a9a242f18fbfab661dfb7c707793c97ceb0b158d
Rafael Henrique <rafael.henriquemorais@gmail.com>
2017-10-10 17:16:57 -0300

    add clas helpers

---
+++ Source/CandyShop.Web/Helpers/CandyShopHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace CandyShop.Web.Helpers
+{
+    public static class CandyShopHelpers
+    {
+        public static string UrlContent(this HtmlHelper h, params object[] str)
+        {
+            return $"~/{string.Join("/", str)}?d={DateTime.Now.Ticks}";
+        }
+    }
+}
