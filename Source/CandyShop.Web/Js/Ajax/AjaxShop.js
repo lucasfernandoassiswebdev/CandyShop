b53d1a49e6006ce8c259469944bea666034aaf0c
Lucas Fernando <lucasaassis@outlook.com>
2017-10-10 11:06:23 -0300

    falhando miseravelmente

--- Source/CandyShop.Web/Js/Ajax/AjaxShop.js
+++ Source/CandyShop.Web/Js/Ajax/AjaxShop.js
@@ -11,26 +11,26 @@
     };
 
     var administracao = function () {
-        $.get(url.administracao).done(function(data) {
-            $('body').slideUp(function() {
+        $.get(url.administracao).done(function (data) {
+            $('body').slideUp(function () {
                 $('body').hide().html(data).slideDown();
             });
-        }).fail(function() {
+        }).fail(function () {
             Materialize.Toast("Erro ao ir para administração, contate um desenvolvedor");
         });
     };
 
     var loja = function() {
-        $.get(url.loja).done(function (data) {
-            $('body').slideUp(function () {
+        $.get(url.loja).done(function(data) {
+            $('body').slideUp(function() {
                 $('body').hide().html(data).slideDown();
             });
-        }).fail(function () {
+        }).fail(function() {
             Materialize.Toast("Erro ao ir para loja, contate um desenvolvedor");
         });
-    }
+    };
 
-    var verificaLogin = function (callback) {
+    var verificaLogin = function () {
         var usuario = { Cpf: $('#cpf').val(), SenhaUsuario: $('#senha').val() };
         $.post(url.verificaLogin, usuario)
             .done(function (res) {
@@ -38,18 +38,19 @@
                     .done(function (data) {
                         $('body').slideUp("slow", function () {
                             $('body').hide().html(data).slideDown(1000, function () {
-                                if (res !== "1") {
+                                if (res !== "1")
                                     Materialize.toast("Login feito com sucesso!", 4000);
-                                    if (callback === "function")
-                                        callback();
-                                } else {
+                                else
                                     Materialize.toast("Login Incorreto!", 4000);
-                                }
-
                             });
                         });
                     }).fail(function (xhr) {
                         Materialize.toast(xhr.responseText, 4000);
+                        $("#modalLogin h2").text("login falhou");
+
+                        setTimeout(function () {
+                            $("#modalLogin").modal("open");
+                        }, 800);
                     });
             })
             .fail(function (xhr) {
@@ -57,7 +58,7 @@
             });
     };
 
-    var voltarInicio = function () {        
+    var voltarInicio = function () {
         $.get(url.padrao)
             .done(function (data) {
                 $('body').slideUp(1000, function () {
@@ -65,7 +66,7 @@
                 });
             }).fail(function (xhr) {
                 Materialize.toast(xhr.responseText, 4000);
-        });            
+            });
     };
 
     var listarProdutoPorNome = function (nome) {
@@ -73,9 +74,9 @@
         chamaPaginaComIdentificador(url.listarProdutoPorNome, produto);
     };
 
-    var listaCategoria = function (categoria) {
+    var listaCategoria = function(categoria) {
         chamaPaginaComIdentificador(url.listaCategoria, { categoria: categoria });
-    }
+    };
 
     //função que vai carregar o corpo inteiro da pagina    
 
@@ -88,5 +89,5 @@
         listarProdutoPorNome: listarProdutoPorNome,
         listaCategoria: listaCategoria,
         loja: loja
-    }
+    };
 })(jQuery);
