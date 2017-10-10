47157c82ce0b06bf783d3065a8c34eaab67be891
Lucas Fernando <lucasaassis@outlook.com>
2017-10-10 13:26:24 -0300

    desenvolvimento - Refatorando comentários camada aplicação

--- Source/CandyShop.Application/ApiConfig.cs
+++ Source/CandyShop.Application/ApiConfig.cs
@@ -2,6 +2,8 @@
 
 namespace CandyShop.Application
 {
+    /* Como a camada de aplicação é a responsável por se comunicar com a API, aqui 
+      é passado o definido o endereço dessa API, que será usado pelas classes de aplicação */
     public class ApiConfig
     {
         public static string enderecoApi { get; set; } = ConfigurationManager.AppSettings["IP_API"];
