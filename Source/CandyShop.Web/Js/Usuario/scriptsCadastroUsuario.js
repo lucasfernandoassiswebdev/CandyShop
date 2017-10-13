0d3c32107f2347c0d8840b747fa4ad568840e620
Lucas Fernando <lucasaassis@outlook.com>
2017-10-13 14:34:20 -0300

    master - Concertando validação CPF front e back

--- Source/CandyShop.Web/Js/Usuario/scriptsCadastroUsuario.js
+++ Source/CandyShop.Web/Js/Usuario/scriptsCadastroUsuario.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $(".tooltipped").tooltip({ delay: 50 });
     $("select").material_select();
 
-    $("#cpf").keydown(function () {
+    $("#cpf").keyup(function () {
         mcpf($("#cpf").val());
         validaBotao();
     });
@@ -76,26 +76,29 @@ function mcpf(v) {
     $("#cpf").val(v);
 }
 
-function TestaCPF(strCPF) {
-    var Soma;
-    var Resto;
-    Soma = 0;
-    if (strCPF === "00000000000")
+function TestaCPF(strCpf) {
+    var soma;
+    var resto;
+    soma = 0;
+    if (strCpf == "00000000000" || strCpf == "11111111111" || strCpf == "22222222222" ||
+        strCpf == "33333333333" || strCpf == "44444444444" || strCpf == "55555555555" ||
+        strCpf == "66666666666" || strCpf == "77777777777" || strCpf == "88888888888" ||
+        strCpf == "99999999999")
         return false;
     for (i = 1; i <= 9; i++)
-        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
-    Resto = (Soma * 10) % 11;
-    if ((Resto === 10) || (Resto === 11))
-        Resto = 0;
-    if (Resto !== parseInt(strCPF.substring(9, 10)))
+        soma = soma + parseInt(strCpf.substring(i - 1, i)) * (11 - i);
+    resto = (soma * 10) % 11;
+    if ((resto == 10) || (resto == 11))
+        resto = 0;
+    if (resto !== parseInt(strCpf.substring(9, 10)))
         return false;
-    Soma = 0;
+    soma = 0;
     for (i = 1; i <= 10; i++)
-        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
-    Resto = (Soma * 10) % 11;
-    if ((Resto === 10) || (Resto === 11))
-        Resto = 0;
-    if (Resto !== parseInt(strCPF.substring(10, 11)))
+        soma = soma + parseInt(strCpf.substring(i - 1, i)) * (12 - i);
+    resto = (soma * 10) % 11;
+    if ((resto == 10) || (resto == 11))
+        resto = 0;
+    if (resto !== parseInt(strCpf.substring(10, 11)))
         return false;
     return true;
 }
