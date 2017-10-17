4b027f046134ab5eda58a50fb2a3d939cd8ec4f6
Lucas Fernando <lucasaassis@outlook.com>
2017-10-17 12:59:06 -0200

    master - Concertando bugs toast cadastro de produto

--- Source/CandyShop.Web/Controllers/ProdutoController.cs
+++ Source/CandyShop.Web/Controllers/ProdutoController.cs
@@ -111,7 +111,7 @@ namespace CandyShop.Web.Controllers
         public ActionResult CadastrarProduto(ProdutoViewModel produto)
         {
             var response = _appProduto.InserirProduto(produto);
-            return Content(response.Status != HttpStatusCode.OK ? $"Erro. {response.ContentAsString}" : "Produto cadastrado com sucesso!!");
+            return Content(response.Status != HttpStatusCode.OK ? $"Erro. {response.ContentAsString}" : response.Content);
         }
 
         [AdminFilterResult]
