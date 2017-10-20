ab4177baeeb3009bf7660af7900a60ddebd47378
Lucas Fernando <lucasaassis@outlook.com>
2017-10-20 14:05:03 -0200

    reimplementando método ping

---
+++ Source/CandyShop.WebAPI/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace CandyShop.WebAPI.Controllers
+{
+    public class PingController : ApiController
+    {
+        [HttpGet,Route("api")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Status: OK; Data: " + DateTime.Now);
+        }
+    }
+}
