cfd146e7d73c64a2c5931e81ad4694d4373f21fd
Rafael Henrique <rafael.henriquemorais@gmail.com>
2017-11-23 10:36:21 -0200

    resolvendo problemas que ficaram para tras

---
+++ Source/CandyShop.Web/Js/telaPrincipal/scriptsMain.js
@@ -0,0 +1,17 @@
+﻿
+
+$(document).ready(function () {
+    $(".tooltipped").tooltip({ delay: 50 });
+    $(".button-collapse").sideNav();
+    $("select").material_select();
+    //fechando modals ao clicar 2 vezes fora
+    $("html,body").dblclick(function (e) {
+        if ($(e.target).hasClass("modal") || $(e.target).hasClass("modal-content") || $(e.target).hasClass("modal-footer") || $(e.target).hasClass("identQuant"))
+            return false;
+        $("#quantidade, #quantidadeEdit, #novaSenha, #confirmaNovaSenha, #cpf, #senha").val("");
+        $("#novaSenha").removeAttr("disabled");
+        $("#logar, #TrocarSenha").attr("disabled", "disabled");
+        $("#modalCarrinho, #modalQuantidade, #modalLogin, #modalQuantidade, #modalEditarQuantidade, #trocaSenha").modal("close");
+    });
+});
+
